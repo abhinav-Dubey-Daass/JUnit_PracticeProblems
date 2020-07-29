969b6407f9f6ccefe8ea3045bf5ab95db289c008
Abhinav Dubey <63160173+abhinav-Dubey-Daass@users.noreply.github.com>
2020-07-29 07:51:20 +0530

    tag JUnit 5 Implementation
    Configuring test cases separately with different tag 'production Tag and development Tag'.

--- TestJava/src/test/java/TestCircleArea.java
+++ TestJava/src/test/java/TestCircleArea.java
@@ -1,13 +1,36 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TestCircleArea {
-@Test
-    public void testComputeCircleArea(){
-    TestAddClass testCircleArea=new TestAddClass();
-    assertEquals(314.197654321555,testCircleArea.computerCircleArea(10),"Should return the right value");
 
-}
+class TestCircleArea {
+    @Test
+    @Tag("production")
+    @DisplayName("Circle Test Area")
+//    @RepeatedTest(value = 3,name ="{displayName}- Repetition {currentRepetition} of  {totalRepetitions}")
+    public void testComputeCircleArea() {
+        TestAddClass testCircleArea = new TestAddClass();
+        boolean assumption=false;
+//        Assumptions.assumeTrue(assumption,"Ignoring Test! Cant run this test");
+        assertEquals(314.197654321555, testCircleArea.computerCircleArea(9), "Should return the right value");
+        System.out.println("success");
+    }
+
+    @Test
+    @Tag("production")
+    public void test11(){
+        assertTrue(true);
+    }
+    @Test
+    @Tag("development")
+    public void test12(){
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("development")
+    public void test13(){
+        assertTrue(true);
+    }
 
 }
